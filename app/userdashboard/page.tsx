4fe8e112aaf17cb3c22b92bcd5825f6a1de81df0
Chamba Nanang <123768840+cypha-io@users.noreply.github.com>
2025-03-06 07:59:22 +0000

    Update page.tsx

--- app/userdashboard/page.tsx
+++ app/userdashboard/page.tsx
@@ -38,9 +38,9 @@ const UserDashboard = () => {
   const [withdrawCurrency, setWithdrawCurrency] = useState("USD");
   const router = useRouter();
 
-  const profit = 288.00;
+  const profit = 654.00;
   const totalInvestment = 2563.00;
-  const totalBalance = profit + totalInvestment;
+  const totalBalance = 3607.00;
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -142,7 +142,7 @@ const UserDashboard = () => {
           <div className="card bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md w-full">
             <FaBitcoin className="text-2xl mb-2 text-primary" />
             <h3 className="text-xl font-semibold mb-2 text-black dark:text-white">BTC Balance</h3>
-            <p className="text-black dark:text-white">0.03052 BTC</p>
+            <p className="text-black dark:text-white">0.03945 BTC</p>
           </div>
           <div className="card bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md w-full">
             <FaChartLine className="text-2xl mb-2 text-primary" />
