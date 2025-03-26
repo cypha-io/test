c1bcdb39f2a4dd3e0c7cf667c5798d4deffc83a1
Chamba Nanang <123768840+cypha-io@users.noreply.github.com>
2025-03-26 07:02:38 +0000

    Update page.tsx

--- app/userdashboard/page.tsx
+++ app/userdashboard/page.tsx
@@ -41,9 +41,9 @@ const UserDashboard = () => {
   const [saveDevice, setSaveDevice] = useState(false);
   const router = useRouter();
 
-  const profit = 6203.00;
-  const totalInvestment = 8974.00;
-  const totalBalance = 15177.00;
+  const profit = 6689.00;
+  const totalInvestment = 9664.00;
+  const totalBalance = 16353.00;
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -173,7 +173,7 @@ const UserDashboard = () => {
           <div className="card bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md w-full">
             <FaExchangeAlt className="text-2xl mb-2 text-primary" />
             <h3 className="text-xl font-semibold mb-2 text-black dark:text-white">Transactions</h3>
-            <p className="text-black dark:text-white">19</p>
+            <p className="text-black dark:text-white">21</p>
           </div>
           <div className="card bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md w-full">
             <FaBalanceScale className="text-2xl mb-2 text-primary" />
@@ -183,7 +183,7 @@ const UserDashboard = () => {
           <div className="card bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md w-full">
             <FaBitcoin className="text-2xl mb-2 text-primary" />
             <h3 className="text-xl font-semibold mb-2 text-black dark:text-white">BTC Balance</h3>
-            <p className="text-black dark:text-white">0.1739 BTC</p>
+            <p className="text-black dark:text-white">0.1873 BTC</p>
           </div>
           <div className="card bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md w-full">
             <FaChartLine className="text-2xl mb-2 text-primary" />
