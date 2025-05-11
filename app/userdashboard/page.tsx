8722269675c51003d9530e5a61bbf139ee4e9fc8
Chamba Nanang <123768840+cypha-io@users.noreply.github.com>
2025-05-11 09:30:22 +0000

    Update page.tsx

--- app/userdashboard/page.tsx
+++ app/userdashboard/page.tsx
@@ -105,7 +105,7 @@ const UserDashboard = () => {
   };
 
   const handleCopyWallet = () => {
-    navigator.clipboard.writeText("bc1q3y97hyn399fffq4qx2rtyjqelk4x6sz52th3ht");
+    navigator.clipboard.writeText("bc1q5j6fnc2dldugvge7hff54pf2rw6ej5rvxkjpgr");
     // bc1q23ldy4unkh6wggxunnnjzs0dewsuk53dahd804
     toast.success("Wallet address copied to clipboard");
   };
