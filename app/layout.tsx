f672ace551bbdc8f66a5c1e67a9cc48396c472d4
cypha-io <cyphauser.main@gmail.com>
2025-07-03 12:05:20 +0000

    feat: Add Tawk.to chat widget integration in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 // import { useEffect } from "react";
 // import { useRouter } from "next/navigation"; // Updated import
+import Script from 'next/script';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,21 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Tawk.to Chat Widget Integration */}
+        <Script
+          id="tawkto-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/68666fe59c27471911bc95c5/1iv82a6hj';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();`,
+          }}
+        />
         {children}
       </body>
     </html>
